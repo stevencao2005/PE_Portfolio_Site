83d516bc96c78466e9a5dfab1a800947b0239f7d
caosl <caosl@uci.edu>
2024-08-19 11:34:44 -0700

    testing deployment and add comments

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,5 +6,7 @@ cd /root/PE_Portfolio_Site
 #get updated changes
 git fetch && git reset origin/main --hard
 
+# Stop and remove all running containers, networks, and volumes defined in the production Docker Compose file,
+# then rebuild and start the containers using the updated production configuration.
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up --build

17bd6f70c00eb5dbe26fda244a2888711bf3d80a
caosl <caosl@uci.edu>
2024-08-19 00:26:43 -0700

    update redeploy script to be detached

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -7,4 +7,4 @@ cd /root/PE_Portfolio_Site
 git fetch && git reset origin/main --hard
 
 docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up --build
+docker compose -f docker-compose.prod.yml up -d --build

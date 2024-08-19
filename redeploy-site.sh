8e39923c571631b76e568150e3ca3aa5c0ba4913
caosl <caosl@uci.edu>
2024-08-18 20:24:54 -0700

    update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,9 +6,5 @@ cd /root/PE_Portfolio_Site
 #get updated changes
 git fetch && git reset origin/main --hard
 
-#activate virtual environment and install the suggested dependencies
-source python3-virtualenv/bin/actviate
-pip install -r requirements.txt
-
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up --build

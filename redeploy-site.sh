bbeef90ae13a1c9f6c8386b579061311417ed233
caosl <caosl@uci.edu>
2024-08-18 18:01:53 -0700

    update redeploy-site.sh script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -10,6 +10,5 @@ git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/actviate
 pip install -r requirements.txt
 
-#reload the changes and restart the service
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up --build
